a521898e5655f6c1dd8510ebab645db3ecc5a3f9
Howard Ding <hding2@illinois.edu>
2018-01-19 15:53:50 -0600

    Use the decorator rather than the helper.

--- app/helpers/file_formats_helper.rb
+++
@@ -1,9 +0,0 @@
-module FileFormatsHelper
-
-  def file_format_extensions_string(file_format)
-    file_format.file_format_profiles.all.collect do |file_format_profile|
-      file_format_profile.logical_extensions.all.collect {|extension| extension.label}
-    end.flatten.uniq.sort.join(', ')
-  end
-
-end
