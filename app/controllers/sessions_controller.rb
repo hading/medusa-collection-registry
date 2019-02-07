cfea391449324c9d43166b0e88657dc96a2b50cc
Howard Ding <hding2@illinois.edu>
2019-02-07 12:02:16 -0600

    First cut at real usage.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -41,25 +41,67 @@ class SessionsController < ApplicationController
   end
 
   def new_saml
-    render plain: 'Login via ruby-saml'
+    request = OneLogin::RubySaml::Authrequest.new
+    redirect_to(request.create(saml_settings))
   end
 
   def create_saml
     render plain: 'Create session via ruby-saml'
   end
 
-  protected
+  def saml_settings
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+    idp_metadata_parser.parse(File.read('/etc/shibboleth/itrust-metadata.xml'),
+                              entity_id: "urn:mace:incommon:uiuc.edu").tap do |settings|
+      settings.soft = false
+      settings.certificate = File.read('/etc/shibboleth/sp-cert.pem')
+      settings.private_key = File.read('/home/lib-medusa-collectionregistry/etc/sp-key.pem')
+      settings.assertion_consumer_service_url = "http://#{request.host}/login_create_saml"
+      #settings.issuer                         = "http://#{request.host}/saml/metadata"
+      settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:transient"
+    end
+    # OneLogin::RubySaml::Settings.new.tap do |settings|
+    #   settings.soft = false
+    #   #settings.assertion_consumer_service_url = "http://#{request.host}/saml/consume"
+    #   #settings.assertion_consumer_service_url = "https://#{request.host}/login_create_saml"
+    #   # settings.issuer                         = "http://#{request.host}/saml/metadata"
+    #   # settings.idp_entity_id                  = "https://app.onelogin.com/saml/metadata/#{OneLoginAppId}"
+    #   # settings.idp_sso_target_url             = "https://app.onelogin.com/trust/saml2/http-post/sso/#{OneLoginAppId}"
+    #   # settings.idp_slo_target_url             = "https://app.onelogin.com/trust/saml2/http-redirect/slo/#{OneLoginAppId}"
+    #   # settings.idp_cert_fingerprint           = OneLoginAppCertFingerPrint
+    #   # settings.idp_cert_fingerprint_algorithm = "http://www.w3.org/2000/09/xmldsig#sha1"
+    #   #settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+    #
+    #   # Optional for most SAML IdPs
+    #   #settings.authn_context = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+    #   # or as an array
+    #   # settings.authn_context = [
+    #   #     "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport",
+    #   #     "urn:oasis:names:tc:SAML:2.0:ac:classes:Password"
+    #   # ]
+    #
+    #   # Optional bindings (defaults to Redirect for logout POST for acs)
+    #   # settings.single_logout_service_binding      = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+    #   # settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
 
-  def clear_and_return_return_path
-    return_url = session[:login_return_uri] || session[:login_return_referer] || root_path
-    session[:login_return_uri] = session[:login_return_referer] = nil
-    reset_ldap_cache(current_user)
-    reset_session
-    return_url
+
   end
 
-  def shibboleth_login_path(host)
-    "/Shibboleth.sso/Login?target=https://#{host}/auth/shibboleth/callback"
-  end
+
+end
+
+protected
+
+def clear_and_return_return_path
+  return_url = session[:login_return_uri] || session[:login_return_referer] || root_path
+  session[:login_return_uri] = session[:login_return_referer] = nil
+  reset_ldap_cache(current_user)
+  reset_session
+  return_url
+end
+
+def shibboleth_login_path(host)
+  "/Shibboleth.sso/Login?target=https://#{host}/auth/shibboleth/callback"
+end
 
 end
